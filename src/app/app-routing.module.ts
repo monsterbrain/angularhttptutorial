192689211c582ccd444346fa365172498695a11c
Faisal rasak <monsterkillu@gmail.com>
2018-12-21 09:43:20 +0530

    added user list , tabs, etc routing added for user list page component ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserlistComponent } from './pages/userlist/userlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // default path
+    component: UserlistComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
